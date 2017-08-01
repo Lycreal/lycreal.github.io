8cd2b33afe5c88746d3f871b2467de133643bbe1
Lycreal <jgsly123@gmail.com>
2017-08-01 19:52:39 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
