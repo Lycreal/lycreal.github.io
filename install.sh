a91a82fce24628e352114de1c1f5c771b55fc5f8
Lycreal <jgsly123@gmail.com>
2017-08-04 14:06:21 +0800

    update zsh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
-source /root/antigen/antigen.zsh
+#!/bin/sh
 
+apt install git zsh
+
+git clone https://github.com/zsh-users/antigen.git ~/.antigen/antigen
+
+cat > ~/.zshrc <<EOF
+source ~/.antigen/antigen/antigen.zsh
 antigen use oh-my-zsh
 antigen bundle command-not-found
 antigen bundle git
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
-
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
-
-antigen theme agnoster
-
 antigen apply
+EOF
